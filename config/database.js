e17ee59f2b9611d5fbcae5de3988d084833605a5
Sofiane Rahab <sofiane.abou.safwan@gmail.com>
2022-05-27 18:59:17 +0100

    fixed issue

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@
 
 // Step 176
 if (process.env.NODE_ENV === "production") {
-  module.exports = require("./prd-database");
+  module.exports = require("./prod-database");
 } else {
   module.exports = require("./dev-database");
 }

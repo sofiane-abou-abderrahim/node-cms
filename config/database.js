a4b898058e084facd6dd22436ee1110769ebbcbd
Sofiane Rahab <sofiane.abou.safwan@gmail.com>
2022-05-28 16:49:50 +0100

    db

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === "production") {
-  module.exports = require("./prod-database");
+  module.exports = require("./prd-database");
 } else {
   module.exports = require("./dev-database");
 }
